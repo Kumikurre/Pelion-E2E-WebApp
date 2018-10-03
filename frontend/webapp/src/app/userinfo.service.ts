5d49dc09f2ec228b98337a6b889d18a5443dc653
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2018-10-03 15:32:28 +0300

    .

--- frontend/webapp/src/app/userinfo.service.ts
+++ frontend/webapp/src/app/userinfo.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Http, Headers, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +7,11 @@ import { Http, Headers, Response } from '@angular/http';
 export class UserinfoService {
 
   constructor(private http: HttpClient) { }
-  headers = new HttpHeaders({
-      'Authorization':
-  });
 
-  apiUrl = "https://api.us-east-1.mbedcloud.com/v3/devices/";
-  H = "Authorization: Bearer ak_1MDE2NWM3YzQ5ZDVlMGEzN2UzYzYwODJmMDAwMDAwMDA016619a9c2fd2200d95670ee00000000awMhr7voDwFFHRNqjOpVKW38FcJN7r02"
+  headers = new HttpHeaders();
+  this.headers = this.headers.set('')
+
+  this.apiUrl = "https://api.us-east-1.mbedcloud.com/v3/devices/";
 
   getUserData() {
     return this.http.get(this.apiUrl + "/v3/accounts/me",);
