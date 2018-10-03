d549eb4606869d8f0ec3d0c5d87767edf2139da6
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2018-10-03 15:37:50 +0300

    add support for custom headers to http

--- frontend/webapp/src/app/userinfo.service.ts
+++ frontend/webapp/src/app/userinfo.service.ts
@@ -8,12 +8,10 @@ export class UserinfoService {
 
   constructor(private http: HttpClient) { }
 
-  headers = new HttpHeaders();
-  this.headers = this.headers.set('')
-
-  this.apiUrl = "https://api.us-east-1.mbedcloud.com/v3/devices/";
+  private headers = new HttpHeaders({'Authorization': 'Bearer PUT KEY HERE'});
+  private apiUrl = "https://api.us-east-1.mbedcloud.com/v3/devices/";
 
   getUserData() {
-    return this.http.get(this.apiUrl + "/v3/accounts/me",);
+    return this.http.get(this.apiUrl + "/v3/accounts/me",{headers: {'Authorization': 'Bearer PUT KEY HERE'}});
   }
 }
