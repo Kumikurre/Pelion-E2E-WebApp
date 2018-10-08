684eae316bb9f2f133f629936a981414e715f9c7
Kumikurre <atte.jauhiainen@gmail.com>
2018-10-08 13:10:31 +0300

    fix angular source files, create class interfaces for rest api

---
+++ frontend/webapp/src/app/models.ts
@@ -0,0 +1,14 @@
+export class UserInfo {
+    
+}
+
+
+export class Device {
+    "data" : [];
+    "total_count" : number;
+    "limit" : number;
+    "after" : string;
+    "has_more" : Boolean;
+    "object" : string;
+    "order" : string;
+  }
