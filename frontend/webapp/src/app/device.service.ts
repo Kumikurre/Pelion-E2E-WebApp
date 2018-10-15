d2c0341573b8438ac13989089820ca1bb26f832e
Kumikurre <atte.jauhiainen@gmail.com>
2018-10-15 13:34:07 +0300

    add device listing to front

--- frontend/webapp/src/app/device.service.ts
+++ frontend/webapp/src/app/device.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Devices } from './models';
+import { apikey } from '../apikey';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private headers = new HttpHeaders({'Authorization': apikey});
+  private apiUrl = 'http://127.0.0.1:5000/Pelion_E2E_Api';
+
+  getDevices() {
+    return this.http.get(this.apiUrl + '/devices', {
+      headers: {
+      'Authorization': apikey
+    }});
+  }
 }
+
+
--- frontend/webapp/src/app/device/device.component.ts
+++ frontend/webapp/src/app/device/device.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Devices } from '../models';
+import { DeviceService } from '../device.service';
+import { apikey } from '../../apikey';
 
 @Component({
   selector: 'app-device',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device.component.css']
 })
 export class DeviceComponent implements OnInit {
-
-  constructor() { }
+  public devicelist;
+  constructor(private deviceservice: DeviceService) { }
 
   ngOnInit() {
+    console.log('Device init');
+    this.getDevices();
   }
 
+  getDevices() {
+    this.deviceservice.getDevices().subscribe((data: Devices) => {this.devicelist = data; });
+    console.log('GetDevices');
+  }
+
+  logStuff() {
+    console.log(this.devicelist);
+  }
 }
